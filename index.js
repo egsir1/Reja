9d11cb62262f277a1e338503b0bb53f6fb4703bd
egsir1 <egsir111@gmail.com>
2023-05-09 01:25:50 +0900

    feat:portfolio-publishing-done

--- index.js
+++ index.js
@@ -11,59 +11,20 @@
 //   console.log("Fayl malumoti =>", data);
 // });
 
-// // externel
-// const moment = require("moment");
-
-// const time = moment().format("HH:mm:ss");
-// console.log(time);
-// const Account = require("./account");
-
-// const myAccount = new Account("Sam", 500000, 4571302458);
-// console.log(myAccount);
-// myAccount.checkBalance();
-// Account.tellTheTime();
-// Account.tellAboutClass();
-// myAccount.withdrawMoney(45000);
-// myAccount.giveDetails();
-// myAccount.makeDeposit(200000);
-// myAccount.withdrawMoney(700000);
-
-//1. init
-
-// nodeApp.use(express.static("public"));
-// nodeApp.use(express.json());
-// nodeApp.use(express.urlencoded({ extended: true }));
-
-// //2 session
-
-// //3
-
-// nodeApp.set("views", "views");
-// nodeApp.set("view engine", "ejs");
-
-// //4
-// nodeApp.get("/", function (req, res) {
-//   res.send("<h1>Our First Express App!</h1>");
-// });
-
-// nodeApp.get("/hello", function (req, res) {
-//   res.end("<h1>Hello World!</h1>");
-// });
-// nodeApp.get("/gift", function (req, res) {
-//   res.end("<h1>Gifts Page </h1>");
-// });
-
-// const server = http.createServer(nodeApp);
-// let PORT = 3000;
-// server.listen(PORT, function () {
-//   console.log("The server is running successfully on port ", PORT);
-// });
-
 // Reja Application
 const express = require("express");
 const nodeApp = express();
-
+const fs = require("fs");
 const http = require("http");
+
+let user;
+fs.readFile("./database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
 
 // Kirish code
 
@@ -84,29 +45,19 @@ nodeApp.post("/create-item", (req, res) => {
   res.json({ test: "success" });
 });
 
+//author route
+
+nodeApp.get("/author", (req, res) => {
+  res.render("author", { user: user });
+});
+
 // get function
 nodeApp.get("/", function (req, res) {
   res.render("shopping");
 });
 
 const server = http.createServer(nodeApp);
-let PORT = 3001;
+let PORT = 3000;
 server.listen(PORT, function () {
   console.log(`The server is running successfully on port : ${PORT}`);
 });
-
-//////////////////////////////////////
-
-const schedule = require("node-schedule");
-
-schedule.scheduleJob("*/5 34 * * * ", function () {
-  console.log("The answer to life, the universe, and everything!");
-  // ringtone.play();
-  // alarnOn = true;
-});
-
-// setTimeout(() => {
-//   if (alarnOn) {
-//     ringtone.pause();
-//   }
-// }, 9000);
--- trains.js
+++ trains.js
@@ -1,2 +1,25 @@
-console.log('This is from trains.js')
+console.log("This is from trains.js");
 
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+let code = 2;
+
+function decodedMsg(message, secret) {
+  const splitInput = message.split(" ");
+
+  console.log(splitInput);
+  for (let i = 0; i >= splitInput.length; i++) {
+    console.log(splitInput[i]);
+  }
+}
+const message = "hello, how are you?";
+
+const secret_msg = decodedMsg(message, code);
+
+// console.log("Secret =>", secret_msg);
+
+// setTimeout(() => {
+//   code *= -1;
+//   const message_encoded = decodedMsg(secret_msg, code);
+//   console.log("Original =>", message_encoded);
+// }, 5000);
