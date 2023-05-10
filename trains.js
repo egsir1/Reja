189f5d0c0544ec9c97b9f8fb6d353aa0a1fbe85e
egsir1 <egsir111@gmail.com>
2023-05-11 07:06:31 +0900

    feat: train => callback hamda async functions

--- trains.js
+++ trains.js
@@ -1,25 +1,80 @@
-console.log("This is from trains.js");
+console.log("Lack Ma maslahatlari");
 
-const alphabet = "abcdefghijklmnopqrstuvwxyz";
+const list = [
+  "Yaxshi talaba boling", //0-20
+  "Togri boshliq tanlash va koproq xato qiling", //20-30
+  "Uzingizga ishlashni boshlang", // 30-40
+  "Siz kuchli bolgan narsalarni qiling", //40-50
+  "Yoshlarga investitsiya qiling", //50-60
+  "Endi dam oling", //60
+];
 
-let code = 2;
+//Callback functions
 
-function decodedMsg(message, secret) {
-  const splitInput = message.split(" ");
-
-  console.log(splitInput);
-  for (let i = 0; i >= splitInput.length; i++) {
-    console.log(splitInput[i]);
+function maslahat(a, callback) {
+  if (typeof a !== "number") callback("Error: please insert a number", null);
+  else if (a <= 20) callback(null, list[0]);
+  else if (a > 20 && a <= 30) callback(null, list[1]);
+  else if (a > 30 && a <= 40) callback(null, list[2]);
+  else if (a > 40 && a <= 50) callback(null, list[3]);
+  else if (a > 50 && a <= 60) callback(null, list[4]);
+  else {
+    setInterval(() => {
+      callback(null, list[5]);
+    }, 2000);
   }
 }
-const message = "hello, how are you?";
 
-const secret_msg = decodedMsg(message, code);
+maslahat(25, (err, data) => {
+  if (err) console.log(err);
+  else {
+    console.log("Javob:", data);
+  }
+});
 
-// console.log("Secret =>", secret_msg);
+////////////////////////////////////////////
+//Async functions
 
-// setTimeout(() => {
-//   code *= -1;
-//   const message_encoded = decodedMsg(secret_msg, code);
-//   console.log("Original =>", message_encoded);
-// }, 5000);
+// async function maslahat(a) {
+//   if (typeof a !== "number") throw new Error("Insert a number");
+//   else if (a <= 20) callback(null, list[0]);
+//   else if (a > 20 && a <= 30) return null, list[1];
+//   else if (a > 30 && a <= 40) return null, list[2];
+//   else if (a > 40 && a <= 50) return null, list[3];
+//   else if (a > 50 && a <= 60) return null, list[4];
+//   else {
+//     // return null, list[5];
+//     return new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         resolve(list[5]);
+//       }, 5000);
+//     });
+//   }
+// }
+
+console.log("Passed here 0");
+
+//then/catch
+// maslahat(25)
+//   .then((data) => {
+//     console.log("Javob:", data);
+//   })
+//   .catch((err) => {
+//     console.log("Error:", err);
+//   });
+//////////////////////////////////////////////////
+
+//async/await
+// async function run() {
+//   let javob = await maslahat(62);
+//   console.log(javob);
+//   javob = await maslahat(33);
+//   console.log(javob);
+//   javob = await maslahat(44);
+//   console.log(javob);
+// }
+// run();
+
+console.log("Passed here 1");
+
+// console.log("This is from trains.js");
